7207d967b6fb8f3e6e3face4150931de5962ca2d
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-08-10 23:54:06 +0300

    feat: create a virtual clipboard class to handle cut,copy and paste

---
+++ src/core/clipboard.ts
@@ -0,0 +1,50 @@
+class VirtualClipboard {
+  private clipboard: string[] = [];
+
+  public copy(originalText: string): void {
+    const selectedText = this.getSelectedText();
+    if (selectedText && this.isSubstring(selectedText, originalText)) {
+      this.copySelectedText(selectedText);
+      console.log("copied", selectedText);
+    }
+    this.periodicClear();
+  }
+
+  public paste(): string | undefined {
+    return this.clipboard.length > 0
+      ? this.clipboard[this.clipboard.length - 1]
+      : undefined;
+  }
+
+  public getHistory(): string[] {
+    return [...this.clipboard];
+  }
+
+  public clear(): void {
+    this.clipboard = [];
+  }
+
+  private periodicClear(): void {
+    const clipboardSize = this.clipboard.length;
+    if (clipboardSize > 100) {
+      this.clipboard = this.clipboard.slice(clipboardSize - 100);
+    }
+  }
+
+  private getSelectedText(): string | undefined {
+    const selection = window.getSelection();
+    return selection && selection.toString().length > 0
+      ? selection.toString()
+      : undefined;
+  }
+
+  private copySelectedText(text: string): void {
+    this.clipboard.push(text);
+  }
+
+  private isSubstring(selectedText: string, originalText: string): boolean {
+    return originalText.includes(selectedText);
+  }
+}
+
+export default new VirtualClipboard();
