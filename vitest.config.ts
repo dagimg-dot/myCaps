8a3b5f4351cadc4ac95ffd7b4596e54792e25eb2
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-08-17 21:30:45 +0300

    chore: set up testing using vitest

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom', // Use jsdom for DOM-related tests
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  }
+});
