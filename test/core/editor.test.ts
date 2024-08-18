eabb1c468a32d982a5b325b00e6fe299d2addf1c
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-08-18 13:48:17 +0300

    task: finish test for moveHorizontal method

--- test/core/editor.test.ts
+++ test/core/editor.test.ts
@@ -7,16 +7,22 @@ describe("TextEditor", () => {
   let editor: TextEditor;
   let mockSelectedText = "";
   let setMockSelection: (start: number, end: number) => void;
+  let setCursorPosition: (position: number) => void;
 
   beforeEach(() => {
     textArea = document.createElement("textarea");
-    textArea.value = `this is a test
-second line`;
+    textArea.value = `first line
+second line
+third line`;
     document.body.appendChild(textArea);
 
     setMockSelection = (start: number, end: number) => {
       mockSelectedText = textArea.value.substring(start, end);
       textArea.setSelectionRange(start, end);
+    };
+
+    setCursorPosition = (position: number) => {
+      textArea.setSelectionRange(position, position);
     };
 
     // Initialize the TextEditor instance
@@ -28,6 +34,8 @@ second line`;
         textArea.selectionEnd = cursorPosition;
       },
     });
+
+    // getCurrentLine() is a private method, so we need to mock it
 
     // Mock window.getSelection()
     // This is because the window.getSelection() method used in the clipboard.ts
@@ -64,7 +72,61 @@ second line`;
     expect(clipboard.getHistory()).toContain(mockSelectedText);
   });
 
-  
+  it("should move the cursor one character to the left (capslock + j)", () => {
+    const curPos = 4;
+    setCursorPosition(curPos);
+
+    editor.moveHorizontal("left");
+    expect(textArea.selectionStart).toBe(curPos - 1);
+    expect(textArea.selectionEnd).toBe(curPos - 1);
+  });
+
+  it("should not move the cursor if it is already at the start of the line", () => {
+    const curPos = 0;
+    setCursorPosition(curPos);
+
+    editor.moveHorizontal("left");
+    expect(textArea.selectionStart).toBe(curPos);
+    expect(textArea.selectionEnd).toBe(curPos);
+  });
+
+  it("should move to the start of the current line if direction is left and start is true (capslock + u)", () => {
+    const middleSecondLinePos = textArea.value.indexOf("second") + 4;
+    const startofSecondLinePos = textArea.value.indexOf("second");
+    setCursorPosition(middleSecondLinePos);
+
+    editor.moveHorizontal("left", true);
+    expect(textArea.selectionStart).toBe(startofSecondLinePos);
+    expect(textArea.selectionEnd).toBe(startofSecondLinePos);
+  });
+
+  it("should move the cursor one character to the right (capslock + l)", () => {
+    const curPos = 4;
+    setCursorPosition(curPos);
+
+    editor.moveHorizontal("right");
+    expect(textArea.selectionStart).toBe(curPos + 1);
+    expect(textArea.selectionEnd).toBe(curPos + 1);
+  });
+
+  it("should not move the cursor if it is already at the end of the line", () => {
+    const curPos = textArea.value.length;
+    setCursorPosition(curPos);
+
+    editor.moveHorizontal("right");
+    expect(textArea.selectionStart).toBe(curPos);
+    expect(textArea.selectionEnd).toBe(curPos);
+  });
+
+  it("should move to the end of the current line if direction is right and start is true (capslock + o)", () => {
+    const middleSecondLinePos = textArea.value.indexOf("second") + 4;
+    const endofSecondLinePos = textArea.value.indexOf("third") - 1;
+    setCursorPosition(middleSecondLinePos);
+
+    editor.moveHorizontal("right", true);
+    expect(textArea.selectionStart).toBe(endofSecondLinePos);
+    expect(textArea.selectionEnd).toBe(endofSecondLinePos);
+  });
 
   afterEach(() => {
     clipboard.clear();
