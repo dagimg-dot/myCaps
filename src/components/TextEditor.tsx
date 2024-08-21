178e1e441342478d8e38718b1a2c98639a9c0013
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-08-22 01:42:04 +0300

    refactor: use virtual keyboard instead of text editor header

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,14 +1,17 @@
 import InputArea from "@/components/InputArea";
 import KeyCapture from "@/components/KeyCapture";
 import LastAction from "@/components/LastAction";
-import TextEditorHeader from "@/components/TextEditorHeader";
+import VirtualKeyboard from "./VirtualKeyboard";
+import Guide from "./Guide";
 
 function TextEditor() {
+  const handleKeyPress = (keys: string[]) => {
+    console.log("Key pressed:", keys);
+  };
   return (
     <div className="p-5 mt-5 text-white rounded-lg bg-opacity-30 bg-neutral-900 backdrop-filter backdrop-blur-lg border-slate-300 border-2 flex flex-col justify-between overflow-y-auto">
-      <div>
-        <TextEditorHeader />
-      </div>
+      <VirtualKeyboard onKeyPress={handleKeyPress} />
+      <Guide />
       <div>
         <InputArea />
         <div className="flex justify-between">
