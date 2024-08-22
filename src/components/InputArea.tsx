18415f5ef2030ad927a7a00150c3afcd44e2607d
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-08-22 23:02:58 +0300

    fix: disable capslock

--- src/components/InputArea.tsx
+++ src/components/InputArea.tsx
@@ -14,7 +14,7 @@ function InputArea() {
     <div className="mt-0 bg-slate-600 bg-opacity-15 p-2 rounded-lg mb-10">
       <Textarea
         ref={textAreaRef}
-        value={text}
+        value={text.toLowerCase()}
         onChange={(e) => setText(e.target.value)}
         className="bg-transparent border-0 h-36 text-lg focus:outline-none placeholder:text-slate-300 placeholder:text-opacity-45"
         placeholder="Type something here..."
